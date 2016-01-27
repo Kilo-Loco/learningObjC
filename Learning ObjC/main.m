949a4e648c4eb04135cd97e7e1b831ffa8b37098
Kyle Lee <kiloloco@me.com>
2016-01-26 23:17:21 -0800

    Initial Commit

---
+++ Learning ObjC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Learning ObjC
+//
+//  Created by Kyle Lee on 1/26/16.
+//  Copyright © 2016 Kilo Loco. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
